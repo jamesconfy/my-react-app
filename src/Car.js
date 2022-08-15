1542abb622bfe61929d3f199ad7612b9c998693d
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-08-15 12:07:45 +0100

    Update

---
+++ src/Car.js
@@ -0,0 +1,5 @@
+function Car(props) {
+    return <h3 className="car">Hi, I am a {props.color} Car, with a brand of {props.brand}!</h3>;
+}
+
+export default Car;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Garage from "./Garage";
 
 // const firstEle = <h1>Hello World!</h1>;
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const myList = ["Array", "Apple", "Pineapple"];
-const lastEle = myList.map((item) => <p>{item}</p>)
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+const sandy = ReactDOM.createRoot(document.getElementById('sandy'));
+// const fandy = ReactDOM.createRoot(document.getElementById('fandy'));
+// const myList = ["Array", "Apple", "Pineapple"];
+// const lastEle = myList.map((item) => <li>{item}</li>);
 
+/* const para = (
+  <table>
+    <thead>
+      <tr>
+        <th>My Table</th>
+        <th>My Table1</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>Hello1</td>
+        <td>Hello2</td>
+
+      </tr>
+
+      <tr>
+        <td>Hello4</td>
+        <td>Hello5</td>
+      </tr>
+
+      <tr>
+        <td>Hello3</td>
+        <td>Hello6</td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+*/
 // root.render(firstEle, lastEle);
-root.render(lastEle);
+// const sandy2 = <p>Hello</p>;
+// const fandy2 = (<option>Wah!</option>)
+
+// root.render(para);
+sandy.render(<Garage />);
+// fandy.render(text);
