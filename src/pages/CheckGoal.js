ceda4721bff99f2b86197b68e808853975d7f881
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-08-15 17:12:00 +0100

    Update

--- src/pages/CheckGoal.js
+++
@@ -1,9 +0,0 @@
-function MadeGoal() {
-    return <h3>Made Goal</h3>;
-}
-
-function MissedGoal() {
-    return <h3>Miss Goal</h3>;
-}
-
-export { MadeGoal, MissedGoal };
--- src/pages/DisplayCars.js
+++
@@ -1,6 +0,0 @@
-function DisplayCars(props) {
-    const brand = props.brand;
-    return <li>{brand}</li>
-}
-
-export default DisplayCars
