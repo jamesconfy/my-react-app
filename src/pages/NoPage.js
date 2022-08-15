8755b99be07d11311bd6c83d995cc8da2662e91f
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-08-15 17:28:25 +0100

    Update

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const NoPage = () => {
-    return <h1>404</h1>;
+    return (
+        <>
+            <h1>404</h1>
+            <Link to="/">Home</Link>
+        </>
+    );
 };
 
 export default NoPage;
