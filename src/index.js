7a325549312dcbc76f9d5a1093e36710cb9ced06
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-08-16 12:46:01 +0100

    almost there

--- src/index.js
+++ src/index.js
@@ -3,13 +3,30 @@ import ReactDOM from "react-dom/client";
 import Garage from "./pages/Garage";
 import Goal from "./pages/Goal";
 import MyForm from "./pages/Form";
+import Timer from "./pages/Timer";
 
 // const firstEle = <h1>Hello World!</h1>;
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const sandy = ReactDOM.createRoot(document.getElementById('sandy'));
-const fandy = ReactDOM.createRoot(document.getElementById('fandy'));
+// const root1 = ReactDOM.createRoot(document.getElementById('root1'));
+// const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+// const root3 = ReactDOM.createRoot(document.getElementById('root3'));
+// const root4 = ReactDOM.createRoot(document.getElementById('root4'));
 // const myList = ["Array", "Apple", "Pineapple"];
 // const lastEle = myList.map((item) => <li>{item}</li>);
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+function App() {
+  const cars = ["Ford", "BMW", "Toyota", "Honda"]
+  return (
+    <>
+      <Goal isGoal={true} />
+      <Garage cars={cars} />
+      <MyForm />
+      <Timer />
+    </>
+  );
+}
+
 
 /* const para = (
   <table>
@@ -44,10 +61,10 @@ const fandy = ReactDOM.createRoot(document.getElementById('fandy'));
 // root.render(firstEle, lastEle);
 // const sandy2 = <p>Hello</p>;
 // const fandy2 = (<option>Wah!</option>)
-
-const cars = ["Ford", "BMW", "Toyota", "Honda"]
 // root.render(para);
-root.render(<Goal isGoal={true} />)
-sandy.render(<Garage cars={cars} />);
-fandy.render(<MyForm />)
+// root1.render(<Goal isGoal={true} />)
+// root2.render(<Garage cars={cars} />);
+// root3.render(<MyForm />)
+// root4.render(<Timer />)
 // fandy.render(text);
+root.render(App)
