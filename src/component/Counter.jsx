46664356eac1e3f45f4b382ef3c5bf5bb92be78d
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-08-18 17:29:52 +0100

    Update

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -5,10 +5,27 @@ function Counter() {
 
   return (
     <>
-      <h4>{count}</h4>
-      <button onClick={(count) => setCount(count) + 1}>Increment</button>
+      <span className={getBadgeClass()}>{count}</span>
+      <button
+        className="btn btn-danger"
+        onClick={() => setCount((count) => count - 1)}
+      >
+        Decrement
+      </button>
+      <button
+        className="btn btn-primary"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        Increment
+      </button>
     </>
   );
+
+  function getBadgeClass() {
+    let classes = "badge text-light m-2 p-2 bg-";
+    classes += count <= 0 ? "warning" : "success";
+    return classes;
+  }
 }
 
 export default Counter;
