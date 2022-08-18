34578ace3d671999bf8b8fbffb99f32a6ba85ce9
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-08-18 14:34:00 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Garage from "./pages/Garage";
-import Goal from "./pages/Goal";
-import MyForm from "./pages/Form";
-import Timer from "./pages/Timer";
-import Calculation from "./pages/Calculation";
-import Ref from "./pages/Ref";
-import Fetch from "./pages/Fetch";
 import "./App.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 function App() {
-  const cars = ["Ford", "BMW", "Toyota", "Honda"]
   return (
     <>
-      <Garage cars={cars} />
-      <MyForm />
-      <Goal isGoal={true} />
-      <Timer />
-      <Calculation />
-      <Ref />
-      <Fetch />
+      <h2 style={{ color: "blue" }}>Starting Again!</h2>
     </>
   );
 }
