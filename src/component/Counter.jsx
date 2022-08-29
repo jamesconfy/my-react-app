398168e60b0333913900e6a5f8f7b7c833edb18f
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-08-29 08:21:44 +0100

    Update counter and counters

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+// import React from "react";
 
 function Counter(props) {
-  const [value, setValue] = useState(props.counter.value);
+  //  const [value, setValue] = useState(props.counter.value);
 
   const renderButton = () => {
-    if (value === 0) {
+    if (props.counter.value === 0) {
       return (
         <button
           className="btn btn-danger btn-sm m-2"
-          onClick={() => setValue((value) => value - 1)}
+          onClick={() => props.onDecrement(props.counter)}
           disabled
         >
           Decrement
@@ -18,7 +18,7 @@ function Counter(props) {
       return (
         <button
           className="btn btn-danger btn-sm m-2"
-          onClick={() => setValue((value) => value - 1)}
+          onClick={() => props.onDecrement(props.counter)}
         >
           Decrement
         </button>
@@ -28,17 +28,17 @@ function Counter(props) {
 
   const getBadgeClass = () => {
     let classes = "badge badge-sm text-light m-2 p-2 bg-";
-    classes += value <= 0 ? "warning" : "success";
+    classes += props.counter.value <= 0 ? "warning" : "success";
     return classes;
   };
 
   return (
     <div>
-      <span className={getBadgeClass()}>{value}</span>
+      <span className={getBadgeClass()}>{props.counter.value}</span>
       {renderButton()}
       <button
         className="btn btn-primary btn-sm m-2"
-        onClick={() => setValue((value) => value + 1)}
+        onClick={() => props.onIncrement(props.counter)}
       >
         Increment
       </button>
--- src/component/counters.jsx
+++ src/component/counters.jsx
@@ -3,24 +3,69 @@ import Counter from "./counter";
 // import { useState } from "react";
 
 function Counters() {
-  const [counters, setCounters] = useState(
-    [
-      { id: 1, value: 7 },
-      { id: 2, value: 19 },
-      { id: 3, value: 200 },
-      { id: 4, value: 17 },
-    ],
-    counters
-  );
+  const [counters, setCounters] = useState([
+    { id: 1, value: 7 },
+    { id: 2, value: 19 },
+    { id: 3, value: 200 },
+    { id: 4, value: 17 },
+  ]);
 
   const onDelete = (counterId) => {
     const counter = counters.filter((c) => c.id !== counterId);
     setCounters(counter);
   };
 
+  const onDecrement = (counter) => {
+    const newCounters = [...counters];
+    const index = counters.indexOf(counter);
+    newCounters[index] = { ...counter };
+    newCounters[index].value--;
+    setCounters(newCounters);
+  };
+
+  const onIncrement = (counter) => {
+    const newCounters = [...counters];
+    const index = counters.indexOf(counter);
+    newCounters[index] = { ...counter };
+    newCounters[index].value++;
+    setCounters(newCounters);
+  };
+
   const Reset = () => {
-    const counter = counters.map((c) => (c.value = 0));
+    const counter = counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
     setCounters(counter);
+  };
+
+  const addBtn = () => {
+    if (counters.length === 0) {
+      return (
+        <button
+          onClick={() =>
+            setCounters((counters) => [...counters, { id: 1, value: 0 }])
+          }
+          className="btn btn-info ml-2"
+        >
+          Add
+        </button>
+      );
+    } else {
+      return (
+        <button
+          onClick={() =>
+            setCounters((counters) => [
+              ...counters,
+              { id: counters[counters.length - 1].id + 1, value: 0 },
+            ])
+          }
+          className="btn btn-info ml-2"
+        >
+          Add
+        </button>
+      );
+    }
   };
 
   return (
@@ -32,23 +77,19 @@ function Counters() {
         >
           Clear
         </button>
-        <button onClick={() => Reset} className="btn btn-danger ml-2">
+        <button onClick={() => Reset()} className="btn btn-danger ml-2">
           Reset
         </button>
-        <button
-          onClick={() =>
-            setCounters((counters) => [
-              ...counters,
-              { id: counters.length + 1, value: 0 },
-            ])
-          }
-          className="btn btn-info ml-2"
-        >
-          Add
-        </button>
+        {addBtn()}
       </div>
       {counters.map((counter) => (
-        <Counter key={counter.id} onDelete={onDelete} counter={counter} />
+        <Counter
+          key={counter.id}
+          onDelete={onDelete}
+          counter={counter}
+          onDecrement={onDecrement}
+          onIncrement={onIncrement}
+        />
       ))}
     </>
   );
